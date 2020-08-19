7b369ad92b98e2cd780f162891814c35e479a595
Sebastiaan Verbeek <sebastiaanverbeek@Sebastiaans-MacBook-Air.local>
2020-08-19 22:17:52 +0700

    Insert noughts and crosses from gameBoard object

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ const gameBoard = (() => {
     let dimension = 3;
 
     const setDimension = (newDimension) => dimension = newDimension;
-
+    
     const get = () => gameBoard;
+
+    const getDimension = () => dimension;
 
     const create = () => {
         const row = [];
@@ -16,26 +18,29 @@ const gameBoard = (() => {
         };
     };
 
-    return { create, get, setDimension };
+    return { create, getDimension, get, setDimension };
 
 })();
 
 const displayController = (() => {
     const main = document.querySelector("#main");
-    main.appendChild(set());
 
     const set = () => {
-        const frame = document.createElement("div");
-        frame.appendChild();
-
-        return frame;
+        for (let i = 0; i < gameBoard.getDimension(); ++i) {
+            for (let j = 0; j < gameBoard.getDimension(); ++j) {
+                const nought = document.createElement("div");
+                nought.className = i.toString() + " " + j.toString();
+                nought.innerHTML = gameBoard.get()[i][j];
+                main.appendChild(nought);
+            }
+        }
     };  
 
     const create = (dimension) => {
         main.innerHTML = "";
         gameBoard.setDimension(dimension);
         gameBoard.create();
-        main.appendChild(set());
+        set();
     };
 
     return { create };
