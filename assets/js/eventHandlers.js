d98d593cb5800a21c7bd963e85f5f25eae6c7647
Sebastiaan Verbeek <sebastiaanverbeek@Sebastiaans-MacBook-Air.local>
2020-09-16 16:25:08 +0700

    feat: place pieces on the board

--- assets/js/eventHandlers.js
+++ assets/js/eventHandlers.js
@@ -1,3 +1,17 @@
 document.getElementById("new-game").addEventListener("click", (e) => {
-    displayController.renderHTML();
+    displayController.renderGameBoardHTML();
+
+    // Need: player1(name, piece), player2(name, piece), firstPlayer
+    // const gamePlayerX = new gamePlayer("x");
+    // const gameplayerO = new gamePlayer("o");
+    document.querySelectorAll(".square").forEach((square) => {
+        square.addEventListener("click", (e) => {
+            gameBoard.move(e.target.id, "x");
+        });
+    });
+        // const firstPlayer = document.getElementById("#select-player");
+        // firstPlayer.innerHTML === "x" ? gameBoard.move("x", this.id) : gameBoard.move("y", this.id);
+
+        // while (!isFinished()) { gameBoard.move()}
 });
+
--- assets/js/modules.js
+++ assets/js/modules.js
@@ -1,38 +1,49 @@
 const gameBoard = (() => {
-    const gameBoard = [];
-    let dimension = 3;
+    const gameBoard = new Array();
+    const dimension = 3;
 
     const get = () => gameBoard;
 
+    const getDimension = () => dimension;
+
     const create = () => {
-        const row = [];
-        for (let i = 0; i < dimension; ++i) {
-            row.push(" ");
-        };
-        for (let i = 0; i < dimension; ++i) {
-            gameBoard.push(row);
-        };
+        for (let i = 0; i < dimension; i++)
+            gameBoard[i] = new Array('', '', '');
     };
 
-    return { create, getDimension, get, setDimension };
+    const move = (coordinates, piece) => {
+        const xy = coordinates.split(".").map(x=>+x);
+        gameBoard[xy[0]][xy[1]] = piece;
+    }
+
+    // const isFinished = () => {
+        
+    // }
+
+    // const setFirstPlayer = () => {
+
+    // }
+
+    return { move, create, getDimension, get };
 
 })();
 
 const displayController = (() => {
     const main = document.querySelector("#game-window");
 
-    const renderHTML = () => {
+    const renderGameBoardHTML = () => {
         gameBoard.create();
         main.innerHTML = "";
         for (let i = 0; i < gameBoard.getDimension(); ++i) {
             for (let j = 0; j < gameBoard.getDimension(); ++j) {
                 const nought = document.createElement("div");
-                nought.className = "square" + " " + i.toString() + " " + j.toString();
+                nought.className = "square" 
+                nought.id = i.toString() + "." + j.toString();
                 nought.innerHTML = gameBoard.get()[i][j];
                 main.appendChild(nought);
             }
         }
     };  
 
-    return { renderHTML };
+    return { renderGameBoardHTML };
 })();
