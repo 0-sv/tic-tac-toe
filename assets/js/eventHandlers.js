765986e98f9de48b8e4690c093d3ee3c826018bf
Sebastiaan Verbeek <sebastiaanverbeek@Sebastiaans-MacBook-Air.local>
2020-09-16 21:37:38 +0700

    feat: player selects X or O

--- assets/js/eventHandlers.js
+++ assets/js/eventHandlers.js
@@ -1,18 +1,10 @@
 document.getElementById("new-game").addEventListener("click", (e) => {
     displayController.renderGameBoardHTML();
-
-    // Need: player1(name, piece), player2(name, piece), firstPlayer
-    // const gamePlayerX = new gamePlayer("x");
-    // const gameplayerO = new gamePlayer("o");
+    const firstPlayerPiece = document.getElementById("player-select-x").checked === true ? "x" : "o";
     document.querySelectorAll(".square").forEach((square) => {
         square.addEventListener("click", (e) => {
-            gameBoard.move(e.target.id, "x");
-            e.target.innerHTML = "x";
+            gameBoard.move(e.target.id, firstPlayerPiece);
+            e.target.innerHTML = firstPlayerPiece;
         });
     });
-        // const firstPlayer = document.getElementById("#select-player");
-        // firstPlayer.innerHTML === "x" ? gameBoard.move("x", this.id) : gameBoard.move("y", this.id);
-
-        // while (!isFinished()) { gameBoard.move()}
 });
-
