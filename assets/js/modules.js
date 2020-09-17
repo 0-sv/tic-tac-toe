28900d97808450f3ded41186ef537262b7ba1a7f
Sebastiaan Verbeek <sebastiaanverbeek@Sebastiaans-MacBook-Air.local>
2020-09-17 11:12:13 +0700

    feat: check horizontal & vertical win

--- assets/js/modules.js
+++ assets/js/modules.js
@@ -11,20 +11,37 @@ const gameBoard = (() => {
             gameBoard[i] = new Array('', '', '');
     };
 
+    function transpose(matrix) {
+        return matrix[0].map((col, i) => matrix.map(row => row[i]));
+    }
+
+    function allEqual(arr, piece) {
+        set = new Set(arr);
+        if (set.size === 1) return set.has(piece) ? true : false;
+        else return false;
+    }
+
+    const pieceWins = (piece) => {
+        const transposedGameBoard = transpose(gameBoard);
+        for (let i = 0; i < dimension; ++i) {
+            if (allEqual(gameBoard[i], piece)) return piece;
+            if (allEqual(transposedGameBoard[i], piece)) return piece;
+        }
+        return false;
+    }
+
+    const gameResult = () => {
+        if (pieceWins("x")) return "x";
+        else if (pieceWins("o")) return "o";
+        else if (gameTies()) return "-";
+    }
+
     const move = (coordinates, piece) => {
         const xy = coordinates.split(".").map(x=>+x);
         gameBoard[xy[0]][xy[1]] = piece;
-    }
+    };
 
-    // const isFinished = () => {
-        
-    // }
-
-    // const setFirstPlayer = () => {
-
-    // }
-
-    return { move, create, getDimension, get };
+    return { move, gameResult, create, getDimension, get };
 
 })();
 
